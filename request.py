d6fe656eb1cbe39f4c5784d08e67363c52fcc4fa
Mike Lang <mikelang3000@gmail.com>
2013-06-16 21:54:25 +1000

    request: Handle auth failure gracefully

--- request.py
+++ request.py
@@ -16,9 +16,16 @@ def request(name, cookies=None, **data):
 	resp = requests.post(url, data=data, cookies=cookies)
 	resp.raise_for_status()
 	resp_obj = decode_json(resp.text)
-	return resp_obj.get('report', None)
+	report = resp_obj.get('report', None)
+
+	if report == 'must_be_logged_in':
+		raise RequestError(report)
+
+	return report
 
 def decode_json(s):
 	"""This is just a helper method to isolate how we turn a response from JSON into python objects."""
 	return loads(s, object_hook=dotdict)
 
+class RequestError(Exception):
+	pass
